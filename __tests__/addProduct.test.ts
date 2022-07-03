c8dae1b5d5854c54c7a32c4f443e9b141026eadc
Dawid Femin <kontakt@dawidfemin.pl>
2022-07-03 22:06:02 +0200

    Created test for addProduct

---
+++ __tests__/addProduct.test.ts
@@ -0,0 +1,7 @@
+import { addProduct } from '../lib/shoppingBasket';
+
+describe("addProduct", () => {
+    it("Add product to basket", () => {
+        expect(addProduct.length).toBeGreaterThan(newProduct.length);
+    });
+  });
